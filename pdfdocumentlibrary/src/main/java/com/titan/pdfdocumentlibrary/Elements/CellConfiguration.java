eefb62e8546161ab56db90d552da0df2e77eb19e
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-20 23:55:43 +0000

    Pdf table cell configuration class

---
+++ pdfdocumentlibrary/src/main/java/com/titan/pdfdocumentlibrary/Elements/CellConfiguration.java
@@ -0,0 +1,13 @@
+package com.titan.pdfdocumentlibrary.Elements;
+
+public class CellConfiguration {
+
+    private int rowspan, colSpan;
+
+
+    public CellConfiguration(){
+
+        this.rowspan = 0;
+        this.colSpan = 0;
+    }
+}
