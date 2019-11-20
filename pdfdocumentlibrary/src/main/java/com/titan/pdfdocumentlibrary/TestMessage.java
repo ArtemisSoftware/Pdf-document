f4fbd527555055dd072b61e88b5af4693fc0876d
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-20 23:36:46 +0000

    Test class

---
+++ pdfdocumentlibrary/src/main/java/com/titan/pdfdocumentlibrary/TestMessage.java
@@ -0,0 +1,14 @@
+package com.titan.pdfdocumentlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestMessage {
+
+    public static void test(Context context, String message){
+
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
