4322d0e083177f4292ed0d0b696d587ab35453af
gustavo <gustavo.maia@b2b.com.pt>
2020-07-07 18:37:36 +0100

    ImageSection

---
+++ app/src/main/java/com/titan/pdfdocument/sections/ImageSection.java
@@ -0,0 +1,37 @@
+package com.titan.pdfdocument.sections;
+
+import android.content.Context;
+
+import com.itextpdf.text.Element;
+import com.titan.pdfdocument.R;
+import com.titan.pdfdocumentlibrary.bundle.Section;
+import com.titan.pdfdocumentlibrary.elements.CellConfiguration;
+import com.titan.pdfdocumentlibrary.elements.Table;
+
+public class ImageSection extends Section {
+
+    private Context context;
+
+    public ImageSection(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected Table getMainTable() {
+        return new Table(3);
+    }
+
+    @Override
+    protected void populateSection() {
+        CellConfiguration cellConfiguration = new CellConfiguration();
+        cellConfiguration.horizontalAlign = Element.ALIGN_MIDDLE;
+        cellConfiguration.verticalAlign = Element.ALIGN_MIDDLE;
+
+
+        table.addCell(context.getResources(), R.drawable.ic_launcher_background);
+        table.addCell(context.getResources(), com.titan.pdfdocumentlibrary.R.drawable.tst_image);
+        table.addCell(context.getResources(), com.titan.pdfdocumentlibrary.R.drawable.tst_image);
+
+
+    }
+}
