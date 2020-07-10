5b5a66d05957b763d4b1978cc685290332781f72
gustavo <gustavo.maia@b2b.com.pt>
2020-07-10 17:40:11 +0100

    PageHeaderfooter

--- app/src/main/java/com/titan/pdfdocument/pages/events/PageHeaderfooter.java
+++ app/src/main/java/com/titan/pdfdocument/pages/events/PageHeaderfooter.java
@@ -1,20 +1,28 @@
 package com.titan.pdfdocument.pages.events;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.ExceptionConverter;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+import com.titan.pdfdocument.sections.HeaderSection;
 
 public class PageHeaderfooter extends PdfPageEventHelper {
 
-//    private Cabecalho cabecalho;
+    private HeaderSection headerSection;
 //    private Rodape rodape;
 //    private boolean ativarCabecalho;
-//    private PdfTemplate template;
-//    private Image total;
+
+    private PdfTemplate template;
+    private Image total;
 
     public PageHeaderfooter(){
 
-//        cabecalho = new Cabecalho();
+        headerSection = new HeaderSection();
 //        rodape = new Rodape();
 //        ativarCabecalho =  false;
 
@@ -23,12 +31,30 @@ public class PageHeaderfooter extends PdfPageEventHelper {
 
     @Override
     public void onOpenDocument(PdfWriter writer, Document document) {
-        super.onOpenDocument(writer, document);
+
+        template = writer.getDirectContent().createTemplate(30, 16);
+        try {
+            total = Image.getInstance(template);
+            total.setRole(PdfName.ARTIFACT);
+        }
+        catch (DocumentException de) {
+            throw new ExceptionConverter(de);
+        }
     }
 
     @Override
     public void onEndPage(PdfWriter writer, Document document) {
-        super.onEndPage(writer, document);
+
+        //header
+
+        //if(ativarCabecalho == true){
+
+        PdfPTable headerTable = headerSection.getSection().getPdfTable();
+        float height = headerTable.getTotalHeight();
+
+        headerTable.writeSelectedRows(0, -1, document.left(), document.top() + ((document.topMargin() + height) / 2), writer.getDirectContent());
+        //}
+
     }
 
     @Override
