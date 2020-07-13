ece1c28b4b78163fd9af4713613d66f1e7e03754
gustavo <gustavo.maia@b2b.com.pt>
2020-07-13 17:12:55 +0100

    MaxTableSection

---
+++ app/src/main/java/com/titan/pdfdocument/sections/MaxTableSection.java
@@ -0,0 +1,26 @@
+package com.titan.pdfdocument.sections;
+
+import com.itextpdf.text.Phrase;
+import com.titan.pdfdocumentlibrary.bundle.Section;
+import com.titan.pdfdocumentlibrary.elements.CellConfiguration;
+import com.titan.pdfdocumentlibrary.elements.FontConfiguration;
+import com.titan.pdfdocumentlibrary.elements.Table;
+
+public class MaxTableSection extends Section {
+    @Override
+    protected Table getMainTable() {
+        return new Table(1);
+    }
+
+    @Override
+    protected void populateSection() {
+
+        CellConfiguration cellConfiguration = new CellConfiguration();
+        FontConfiguration fontConfiguration = new FontConfiguration();
+
+        for(int index = 0; index < 9; ++index){
+
+            table.addLine(new Phrase("I am line number " + (index + 1), fontConfiguration.getFont(8)), cellConfiguration);
+        }
+    }
+}
