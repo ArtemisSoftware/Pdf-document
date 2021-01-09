c550f7b410c25c0650971524921cf5d33edccd2b
gustavo <gustavo.isaac@gmail.com>
2021-01-09 20:06:53 +0000

    Pdf viewer for base64 data

--- app/src/main/java/com/titan/pdfdocument/PdfViewerActivity.java
+++ app/src/main/java/com/titan/pdfdocument/PdfViewerActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PdfViewerActivity extends AppCompatActivity {
 
@@ -18,11 +26,60 @@ public class PdfViewerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pdf_viewer);
 
 
-
         pdfView = ((PDFView)findViewById(R.id.pdfView));
 
-        Uri uri =  Uri.parse( "http://www.africau.edu/images/default/sample.pdf" );
-        pdfView.fromUri(uri)
-                .load();
+        getBigDocument();
     }
+
+
+    private void getSmallDocument(){
+
+        String example = getString(R.string.example_1);
+
+        byte[] encodedBytes = Base64.decode(example, Base64.CRLF);
+        pdfView.fromBytes(encodedBytes)
+                .swipeHorizontal(true)
+                .spacing(10)
+                .load();
+
+
+    }
+
+
+    private void getBigDocument(){
+
+        try {
+            JSONObject example_ = new JSONObject(getTermsString());
+
+            byte[] encodedBytes = Base64.decode(example_.getString("document"), Base64.CRLF);
+            pdfView.fromBytes(encodedBytes)
+                    .swipeHorizontal(true)
+                    .spacing(10)
+                    .load();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String getTermsString() {
+        StringBuilder termsString = new StringBuilder();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("pdf_cgd.txt")));
+
+            String str;
+            while ((str = reader.readLine()) != null) {
+                termsString.append(str);
+            }
+
+            reader.close();
+            return termsString.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
